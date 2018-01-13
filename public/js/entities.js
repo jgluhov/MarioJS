3c9e07ae16c724c1778f5101c10e10f2cdb9e16d
jgluhov <jgluhov@gmail.com>
2018-01-13 23:00:53 +0300

    add mario sprite

--- public/js/entities.js
+++ public/js/entities.js
@@ -6,20 +6,26 @@ import {createAnimation} from './animation.js';
 import {SLOW_DRAG, FAST_DRAG} from "./constants.js";
 
 export function createMarioEntity() {
-    return loadSpriteSheet('characters')
+    return loadSpriteSheet('mario-characters')
         .then(sprite => {
             const mario = new Entity();
-            mario.size.set(20, 32);
+            mario.size.set(50, 51);
 
-            const runAnimation = createAnimation(['run-1', 'run-2', 'run-3'], 17);
+            const runAnimation = createAnimation([
+                'run-1', 'run-2', 'run-3', 'run-4',
+                'run-5', 'run-6', 'run-7', 'run-8',
+                'run-9', 'run-10', 'run-11'
+            ], 7);
 
             function routeFrame(mario) {
+
                 if (mario.jump.falling) {
                     return 'jump';
                 }
 
                 if (mario.run.distance > 0) {
-                    if ((mario.vel.x > 0 && mario.run.direction < 0) || (mario.vel.x < 0 && mario.run.direction > 0)) {
+                    if ((mario.vel.x > 0 && mario.run.direction < 0) ||
+                        (mario.vel.x < 0 && mario.run.direction > 0)) {
                         return 'break'
                     }
                     return runAnimation(mario.run.distance);
